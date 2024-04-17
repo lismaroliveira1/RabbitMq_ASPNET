456c1df1401a9229e142cb76d4cc105d77c7094e
Lismar Oliveira <englismaroliveira@gmail.com>
2024-04-17 09:45:02 -0300

    add person entity

---
+++ src/services/Client/Client.Domain/Entity/Person.cs
@@ -0,0 +1,20 @@
+namespace Client.Domain.Entities;
+
+public class Person : Base
+{
+    public string Name {get; set;}
+    public string Age {get; set;}
+    public string Role {get; set;}
+    public string Document {get; set;}
+
+    public override bool Validate()
+    {
+        throw new NotImplementedException();
+    }
+
+   public static class UserRole 
+    {
+       public static readonly string Admin = "ADMIN";
+       public static readonly string Default = "DEFAULT";
+    }
+}
