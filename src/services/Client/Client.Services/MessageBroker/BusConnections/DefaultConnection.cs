3ce5d1f0874fc3f8d3deb7591ded34d1750e59eb
Lismar Oliveira <englismaroliveira@gmail.com>
2024-04-17 12:41:52 -0300

    add default connection

---
+++ src/services/Client/Client.Services/MessageBroker/BusConnections/DefaultConnection.cs
@@ -0,0 +1,107 @@
+using System.Net.Sockets;
+using Client.Infrastructure.Interfaces;
+using Polly;
+using Polly.Retry;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Exceptions;
+
+namespace Client.Infrastructure.BusConnections;
+
+public class DefaultConnection : IMessageBrokerPersistentConnection
+{
+    private readonly IConnectionFactory _connectionFactory;
+        private IConnection _connection;
+        private readonly int _retryCount;
+        private bool _disposed;
+
+        public DefaultConnection(IConnectionFactory connectionFactory,
+                                                   int retryCount)
+        {
+            _connectionFactory = connectionFactory;
+            _retryCount = retryCount;
+        }
+
+        public bool IsConnected
+        {
+            get
+            {
+                return _connection != null && _connection.IsOpen && !_disposed;
+            }
+        }
+        public bool TryConnect()
+        {
+            var policy = RetryPolicy.Handle<SocketException>()
+                .Or<BrokerUnreachableException>()
+                .WaitAndRetry(_retryCount, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), (ex, time) =>
+                  {
+                      //logging
+                  });
+            policy.Execute(() =>
+            {
+                _connection = _connectionFactory.CreateConnection();
+            });
+
+            if (IsConnected)
+            {
+                _connection.ConnectionShutdown += OnConnectionShutdown;
+                _connection.CallbackException += OnCallbackException;
+                _connection.ConnectionBlocked += OnConnectionBlocked;
+
+                return true;
+            }
+            else
+            {
+                //logging
+                return false;
+            }
+        }
+
+        private void OnConnectionBlocked(object? sender, ConnectionBlockedEventArgs e)
+        {
+            if (_disposed) return;
+            //logging
+            TryConnect();
+        }
+
+        private void OnCallbackException(object? sender, CallbackExceptionEventArgs e)
+        {
+            if (_disposed) return;
+            //logging
+            TryConnect();
+        }
+
+        private void OnConnectionShutdown(object? sender, ShutdownEventArgs reason)
+        {
+            if (_disposed) return;
+            //logging
+            TryConnect();
+        }
+
+        public IModel CreateModel()
+        {
+            if (!IsConnected)
+            {
+                //logging
+                throw new InvalidOperationException("No RabbitMQ connections are available to perfrom this action.");
+            }
+
+            return _connection.CreateModel();
+        }
+
+        public void Dispose()
+        {
+            if (_disposed) return;
+
+            _disposed = true;
+
+            try
+            {
+                _connection.Dispose();
+            }
+            catch (Exception ex)
+            {
+                //logging
+            }
+        }
+}
