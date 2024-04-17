96bf84f65d499755abceba58d3210b0723e286b8
Lismar Oliveira <englismaroliveira@gmail.com>
2024-04-17 10:18:07 -0300

    add base CRUD interface

---
+++ src/services/Client/Client.Infrastructure/Interfaces/IBaseInterface.cs
@@ -0,0 +1,10 @@
+using System.Buffers.Text;
+using Client.Domain.Entities;
+
+namespace Client.Infrastructure.Interfaces;
+public interface IBaseRepository<T> where T : Base {
+    Task<T> Get(long id);
+    Task<T> Create(T obj);
+    Task<T> Update(T obj);
+    Task<T> Delete(T obj);
+}
