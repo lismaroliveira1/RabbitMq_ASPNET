76daa060d94232c0f11418c52f31e67f50bf8cd6
Lismar Oliveira <englismaroliveira@gmail.com>
2024-04-17 09:55:47 -0300

    fix age field type

--- src/services/Client/Client.Domain/Entity/Person.cs
+++ src/services/Client/Client.Domain/Entity/Person.cs
@@ -3,7 +3,7 @@ namespace Client.Domain.Entities;
 public class Person : Base
 {
     public string Name {get; set;}
-    public string Age {get; set;}
+    public int Age {get; set;}
     public string Role {get; set;}
     public string Document {get; set;}
 
