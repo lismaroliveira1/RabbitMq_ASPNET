88582b31d1be2695538cee86a38ca1f1a9fefea0
Lismar Oliveira <englismaroliveira@gmail.com>
2024-04-17 11:59:02 -0300

    add response message obj

---
+++ src/services/Client/Client.Services/MessageBroker/Model/Response.cs
@@ -0,0 +1,12 @@
+﻿using User.Domain.Entities;
+
+namespace Client.Service.MessageBroker.Model
+{
+    public class Response
+    {
+        public bool Success { get; set; }
+        public string ErrorCode { get; set; }
+        public string Message { get; set; }
+        public Partner Payload {get; set;}
+    }
+}
