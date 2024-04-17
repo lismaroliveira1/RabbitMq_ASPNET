5eef9645665c96ecb0d412b64d16d638bbb33bc8
Lismar Oliveira <englismaroliveira@gmail.com>
2024-04-17 10:59:41 -0300

    Fix person Id ambiguity

--- src/services/Client/Client.Domain/Entity/Base.cs
+++ src/services/Client/Client.Domain/Entity/Base.cs
@@ -2,11 +2,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Client.Domain.Entities;
 
-public abstract class Base {
+public abstract  class Base {
     [Key]
     public long Id {get; set;}
-    public DateTime CreateAt {get; set;}
-    public DateTime Id {get; set;}
+    public DateTime? CreateAt {get; set;} = DateTime.Now;
+    public DateTime? UpdatedAt {get; set;} = DateTime.Now;
     internal List<string> _errors;
     public IReadOnlyCollection<string>? Errors => _errors;
     public abstract bool Validate();
