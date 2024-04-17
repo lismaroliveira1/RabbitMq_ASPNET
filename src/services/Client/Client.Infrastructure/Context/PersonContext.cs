731134c6cbe19f78c58de87b3f8dab9d32fe960d
Lismar Oliveira <englismaroliveira@gmail.com>
2024-04-17 11:00:27 -0300

    add person context

---
+++ src/services/Client/Client.Infrastructure/Context/PersonContext.cs
@@ -0,0 +1,28 @@
+﻿using Client.Domain.Entities;
+using Client.Infrastructure.Mappings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Client.Infrastructure.Context;
+
+public class PersonContext :DbContext
+{
+    public IConfiguration _configuration { get; }
+
+    public PersonContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public PersonContext(DbContextOptions options) : base(options) { }
+
+    public virtual DbSet<Person> Partners { get; set; }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfiguration(new PersonMap());
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(_configuration.GetConnectionString("DefaultConnection"));
+    }
+}
