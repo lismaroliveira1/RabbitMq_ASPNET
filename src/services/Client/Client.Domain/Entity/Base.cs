76a90c9d5864c14f5322c30f721f9e49163c88c2
Lismar Oliveira <englismaroliveira@gmail.com>
2024-04-17 10:37:03 -0300

    add fields to table

--- src/services/Client/Client.Domain/Entity/Base.cs
+++ src/services/Client/Client.Domain/Entity/Base.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Client.Domain.Entities;
 
 public abstract class Base {
+    [Key]
     public long Id {get; set;}
+    public DateTime CreateAt {get; set;}
+    public DateTime Id {get; set;}
     internal List<string> _errors;
     public IReadOnlyCollection<string>? Errors => _errors;
     public abstract bool Validate();
