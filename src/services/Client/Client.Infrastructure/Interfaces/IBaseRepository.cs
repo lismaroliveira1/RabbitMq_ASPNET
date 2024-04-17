a3b284fa0a391833f34c53c274a61e78c8e56b3e
Lismar Oliveira <englismaroliveira@gmail.com>
2024-04-17 11:07:16 -0300

    remove return function from delete method

--- src/services/Client/Client.Infrastructure/Interfaces/IBaseRepository.cs
+++ src/services/Client/Client.Infrastructure/Interfaces/IBaseRepository.cs
@@ -6,5 +6,5 @@ public interface IBaseRepository<T> where T : Base {
     Task<T> Get(long id);
     Task<T> Create(T obj);
     Task<T> Update(T obj);
-    Task<T> Delete(T obj);
+    Task Delete(long id);
 }
