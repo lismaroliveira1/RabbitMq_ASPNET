b17c9eb1e1e23b691dd90d94322bc595328e80ce
Lismar Oliveira <englismaroliveira@gmail.com>
2024-04-18 08:49:57 -0300

    add response model

---
+++ src/services/ServerLogger/ServerLogger.Infra/Models/Response.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ServerLogger.Infra.Models
+{
+    public class Response
+    {
+        public bool Success { get; set; }
+        public string ErrorCode { get; set; }
+        public string Message { get; set; }
+        public dynamic Payload {get; set;}
+    }
+}
