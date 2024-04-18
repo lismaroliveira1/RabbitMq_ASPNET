e076dfcefb9c42233d150358aaf4dce6c45f0c57
Lismar Oliveira <englismaroliveira@gmail.com>
2024-04-18 16:47:42 -0300

    add order entity

--- src/services/Order/Order.Domain/Entities/Order.cs
+++ src/services/Order/Order.Domain/Entities/Order.cs
@@ -1,7 +1,20 @@
 namespace Order.Domain.Entities;
 public class Order : Base
 {
+
+    public long OrderId { get; private set; }
+    public string OrderStatus { get; private set; }
+    public long Person {get; private set;}
     
+    protected Order() {}
+
+    public Order(long orderId, string orderStatus, long person)
+    {
+        OrderId = orderId;
+        OrderStatus = orderStatus;
+        Person = person;
+    }
+
     public override bool Validate()
     {
         throw new NotImplementedException();
