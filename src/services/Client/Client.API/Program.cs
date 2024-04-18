f87cb44e66d5762670de86b337acd3a63e3eea9d
Lismar Oliveira <englismaroliveira@gmail.com>
2024-04-18 08:06:05 -0300

    set entry configurations

--- src/services/Client/Client.API/Program.cs
+++ src/services/Client/Client.API/Program.cs
@@ -1,13 +1,47 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(ctx =>  {
+    ctx.SwaggerDoc("v1", new OpenApiInfo {
+        Title = "Async Communication",
+        Version = "v1",
+        Description = "This project is builded in order to provide configurations regarding asynchronous communication between microservices ",
+        Contact = new OpenApiContact {
+            Name = "Lismar Oliveira",
+            Email ="englismaroliveira@gmail.com",
+            Url = new Uri("https://www.linkedin.com/in/lismar-oliveira-9a93ba94/")
+        }
+    });
+
+    ctx.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme {
+        In = ParameterLocation.Header,
+        Description = "The bearer token is mandatory.",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    
+    ctx.AddSecurityRequirement(new OpenApiSecurityRequirement()
+      {
+        {    
+          new OpenApiSecurityScheme
+          {
+            Reference = new OpenApiReference
+              {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+              }
+            },
+            new List<string>()
+          }
+        });
+});
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -16,29 +50,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
