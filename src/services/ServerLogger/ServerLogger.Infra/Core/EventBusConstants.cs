fbd04ba6a304d739b12a4f9744407d5c5567ca03
Lismar Oliveira <englismaroliveira@gmail.com>
2024-04-18 08:51:37 -0300

    add Event bus constants

---
+++ src/services/ServerLogger/ServerLogger.Infra/Core/EventBusConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace ServerLogger.Infra.Core
+{
+    public static class EventBusConstants
+    {
+        public const string DirectQueue = "direct";
+        public const string RdcReplyQueue = "rdcReply";
+        public const string LogsQueue = "log";
+        public const string RdcPublishQueue = "rdcPublish";
+    }
+}
