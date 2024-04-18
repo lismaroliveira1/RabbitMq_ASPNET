7cd76c8be3322410fafa28df0c6ed3f4839e1e4c
Lismar Oliveira <englismaroliveira@gmail.com>
2024-04-18 16:41:35 -0300

    add fields to base entity

--- src/services/Order/Order.Domain/Entities/Base.cs
+++ src/services/Order/Order.Domain/Entities/Base.cs
@@ -3,6 +3,8 @@ namespace Order.Domain.Entities;
 public abstract class Base
 {
     public long Id { get; set; }
+    public DateTime CreatedAt {get; set;}
+    public DateTime UpdateAt {get; set;}
     internal List<string>? _errors;
     public IReadOnlyCollection<string>? Errors => _errors;
     public abstract bool Validate();
---
+++ src/services/Order/Order.Domain/Entities/Order.cs
@@ -0,0 +1,9 @@
+namespace Order.Domain.Entities;
+public class Order : Base
+{
+    
+    public override bool Validate()
+    {
+        throw new NotImplementedException();
+    }
+}
