1471bb77374ac2e6ba23a222bd00fae921d7ebbb
Lismar Oliveira <englismaroliveira@gmail.com>
2024-04-19 17:44:18 -0300

    add sweagger api descriptions

--- src/services/Order/Order.API/Program.cs
+++ src/services/Order/Order.API/Program.cs
@@ -1,9 +1,38 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(ctx =>  {
+    ctx.SwaggerDoc("v1", new OpenApiInfo {
+        Title = "Challenge API",
+        Version = "v1",
+        Description = "The is API is builded for win the challenge",
+        Contact = new OpenApiContact {
+            Name = "Lismar Oliveira",
+            Email ="englismarOliveira@gmail.com",
+            Url = new Uri("https://www.linkedin.com/in/lismar-oliveira-9a93ba94/")
+        }
+    });
+    
+    ctx.AddSecurityRequirement(new OpenApiSecurityRequirement()
+      {
+        {    
+          new OpenApiSecurityScheme
+          {
+            Reference = new OpenApiReference
+              {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+              }
+            },
+            new List<string>()
+          }
+        });
+});
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -15,5 +44,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
