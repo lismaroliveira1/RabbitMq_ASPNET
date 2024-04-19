a6cd715c9dcfe309c42b441a31fbfd553d7f5dea
Lismar Oliveira <englismaroliveira@gmail.com>
2024-04-19 11:04:45 -0300

    add model response

---
+++ src/services/Order/Order.Services/MessageBroker/Model/Response.cs
@@ -0,0 +1,12 @@
+﻿using Order.Domain.Entities;
+
+namespace Client.Service.MessageBroker.Model
+{
+    public class Response
+    {
+        public bool Success { get; set; }
+        public string ErrorCode { get; set; }
+        public string Message { get; set; }
+        public OrderEntity Payload {get; set;}
+    }
+}
