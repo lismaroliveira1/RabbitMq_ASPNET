0a3304dd81e8f7444ebf9f98b914dd3bec93a2f3
Lismar Oliveira <englismaroliveira@gmail.com>
2024-04-19 09:21:07 -0300

    remove unless field

--- src/services/Order/Order.Domain/Entities/Order.cs
+++ src/services/Order/Order.Domain/Entities/Order.cs
@@ -5,7 +5,6 @@ namespace Order.Domain.Entities;
 public class OrderEntity : Base
 {
 
-    public long OrderId { get; private set; }
     public string OrderStatus { get; private set; }
     public long Person {get; private set;}
     
@@ -13,7 +12,6 @@ public class OrderEntity : Base
 
     public OrderEntity(long orderId, string orderStatus, long person)
     {
-        OrderId = orderId;
         OrderStatus = orderStatus;
         Person = person;
     }
